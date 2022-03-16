fbb309803383026b5aeedc80db82936de2f1bd44
Felipe Miranda Torres De La Llosa <felipe.miranda@fing.edu.uy>
2022-03-16 12:51:13 -0300

    Upload New File

---
+++ Ejercicio_2/B.cpp
@@ -0,0 +1,13 @@
+#include "include/B.h"
+#include<iostream>
+using namespace std;
+
+ClassB::ClassB(int b, ClassA * A, ClassC * C){
+    this->b = b; 
+    myA = A;
+    myC = C;
+}
+
+void ClassB::printInt(){
+    cout << b;
+}

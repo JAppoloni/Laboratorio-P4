8fc0fc998037789a836f9ccdd5c524a8ab54c349
Juan Appoloni <juan.appoloni@fing.edu.uy>
2022-03-16 15:31:38 -0300

    no message

--- Laboratorio 0/Ejercicio_1/Common/Clases/Habitacion.cpp
+++ Laboratorio 0/Ejercicio_1/Common/Clases/Habitacion.cpp
@@ -1,37 +1,56 @@
 #include "header/Habitacion.hpp"
 #include <iostream>
 
-int Habitacion::getNumero() { 
-    return this->numero; 
+int Habitacion::getNumero()
+{
+    return this->numero;
 }
 
-void Habitacion::setNumero(int numero) { 
-    this->numero = numero; 
-}
-
-float Habitacion::getPrecio() { 
-    return this->precio; 
-}
-
-void Habitacion::setPrecio(float precio) { 
-    this->precio = precio; 
-}
-
-int Habitacion::getCapacidad() { 
-    return this->capacidad; 
-}
-
-void Habitacion::setCapacidad(int capacidad) { 
-    this->capacidad = capacidad; 
-}
-
-Habitacion::Habitacion(int numero, float precio, int capacidad) {
+void Habitacion::setNumero(int numero)
+{
     this->numero = numero;
-    this->capacidad = capacidad;
+}
+
+float Habitacion::getPrecio()
+{
+    return this->precio;
+}
+
+void Habitacion::setPrecio(float precio)
+{
     this->precio = precio;
 }
 
-void Habitacion::imprimir() {
+int Habitacion::getCapacidad()
+{
+    return this->capacidad;
+}
+
+void Habitacion::setCapacidad(int capacidad)
+{
+    this->capacidad = capacidad;
+}
+
+Reserva *Habitacion::getListaReservas()
+{
+    return this->listaReservas;
+}
+void Habitacion::setListaReservas(Reserva *listaReservas)
+{
+    delete[] this->listaReservas;
+    this->listaReservas = listaReservas;
+}
+
+Habitacion::Habitacion(int numero, float precio, int capacidad, Reserva *listaReservas)
+{
+    this->numero = numero;
+    this->capacidad = capacidad;
+    this->precio = precio;
+    this->listaReservas = listaReservas;
+}
+
+void Habitacion::imprimir()
+{
     std::cout << " La habitación número" << this->numero << " cuenta con una capacidad de " << this->capacidad << " a un precio de  $ " << this->precio << ".";
 }
 
--- Laboratorio 0/Ejercicio_1/Common/Clases/header/Habitacion.hpp
+++ Laboratorio 0/Ejercicio_1/Common/Clases/header/Habitacion.hpp
@@ -1,13 +1,15 @@
 #ifndef _HABITACION_HH_
 #define _HABITACION_HH_
 
-class Habitacion {
+#include "Reserva.hpp"
+
+class Habitacion
+{
 private:
     int numero;
-
     float precio;
-
     int capacidad;
+    Reserva *listaReservas;
 
 public:
     void imprimir();
@@ -15,13 +17,15 @@ public:
     int getNumero();
     float getPrecio();
     int getCapacidad();
+    Reserva *getListaReservas();
 
+    void setListaReservas(Reserva *listaReservas);
     void setNumero(int numero);
     void setPrecio(float precio);
     void setCapacidad(int capacidad);
 
     Habitacion();
-    Habitacion(int numero, float precio, int capacidad);
+    Habitacion(int numero, float precio, int capacidad, Reserva *listaReservas);
     ~Habitacion();
 };
 

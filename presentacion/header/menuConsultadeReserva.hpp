23f5b4c8790d34776ec0487a5752970bf67dd06a
Carolina Martinez <carolina.martinez@fing.edu.uy>
2022-06-19 02:45:56 -0300

    menu de Consulta de Reserva

---
+++ presentacion/header/menuConsultadeReserva.hpp
@@ -0,0 +1,13 @@
+#ifndef MENU_CONSULTA_RESERVA_H
+#define MENU_CONSULTA_RESERVA_H
+
+#include "../../common/datatype/header/DTHostal.hpp"
+#include "../../dominio/header/Fabrica.hpp"
+
+#include "utils.hpp"
+#include <iostream>
+using namespace std;
+
+void menuConsultadeReserva();
+
+#endif
